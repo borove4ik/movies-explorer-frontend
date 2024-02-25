598f64a695cfebaf3f3cddccb3ce54944378b09c
Denis <123562174+borove4ik@users.noreply.github.com>
2024-02-25 20:48:30 +0300

    сброс стилей списку

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -7,19 +7,19 @@ const Portfolio = () => {
       <section className="portfolio">
         <h2 className="portfolio__title">Портфолио</h2>
         <ul>
-          <li> 
+          <li className="portfolio__marker"> 
             <a className="portfolio__link" href="https://github.com/borove4ik/how-to-learn" target="_blank" rel="noreferrer">
               <p className="portfolio__link-text">Статичный сайт</p>
               <img src={arrow} alt="иконка ссылки" className="portfolio__link-icon" /> 
             </a>
           </li>
-          <li>
+          <li className="portfolio__marker">
             <a className="portfolio__link" href="https://github.com/borove4ik/russian-travel" target="_blank" rel="noreferrer">
               <p className="portfolio__link-text">Адаптивный сайт</p>
               <img src={arrow} alt="иконка ссылки" className="portfolio__link-icon" /> 
             </a>
           </li>
-          <li>
+          <li className="portfolio__marker">
             <a className="portfolio__link portfolio__link_borderless" href="https://github.com/borove4ik/react-mesto-api-full-gha" target="_blank" rel="noreferrer">
               <p className="portfolio__link-text">Одностраничное приложение</p>
               <img src={arrow} alt="иконка ссылки" className="portfolio__link-icon" />  
